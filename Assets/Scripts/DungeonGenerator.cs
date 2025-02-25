c85e4762146d0f7b04dfd7ff7bacee66c78e0431
File_Missing <michaverheij@gmail.com>
2025-02-25 16:41:39 +0100

    basic room splitting functionality

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,16 +1,81 @@
+using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int seed;
+
+    public Vector2Int size = new Vector2Int(100, 50);
+    public Vector2Int maxRoomSize = new Vector2Int(10, 10);
+    List<RectInt> roomsToCheck = new List<RectInt>();
+    public int splitAmount = 3;
+
+    private void Start()
     {
-        
+        Random.InitState(seed);
+
+        RectInt startRoom = new RectInt(0, 0, size.x, size.y);
+        roomsToCheck.Add(startRoom);
+
+        for (int i = 0; i < splitAmount; i++)
+        {
+            List<RectInt> newRooms = new List<RectInt>();
+            foreach (RectInt room in roomsToCheck)
+            {
+                bool splitHorizontal = Mathf.Round(Random.value) == 1;
+                if (splitHorizontal) newRooms.AddRange(SplitHorizontally(room));
+                else newRooms.AddRange(SplitVertically(room));
+
+                //splitHorizontally = !splitHorizontally;
+            }
+            roomsToCheck = newRooms;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        DrawRooms();
+    }
+
+    RectInt[] SplitVertically(RectInt room)
+    {
+        RectInt[] newRooms = new RectInt[2];
+
+        RectInt newRoom1 = new RectInt(room.x, room.y, room.width / 2, room.height);
+        RectInt newRoom2 = new RectInt(room.x + room.width / 2 - 1, room.y, room.width - newRoom1.width + 1, room.height);
+
+        newRooms[0] = newRoom1;
+        newRooms[1] = newRoom2;
+
+        return newRooms;
+    }
+
+    RectInt[] SplitHorizontally(RectInt room)
+    {
+        RectInt[] newRooms = new RectInt[2];
+
+        RectInt newRoom1 = new RectInt(room.x, room.y, room.width, room.height / 2);
+        RectInt newRoom2 = new RectInt(room.x, room.y + room.height / 2 - 1, room.width, room.height - newRoom1.height + 1);
+
+        newRooms[0] = newRoom1;
+        newRooms[1] = newRoom2;
+
+        return newRooms;
+    }
+
+    void DrawRooms()
+    {
+        foreach (RectInt room in roomsToCheck)
+        {
+            AlgorithmsUtils.DebugRectInt(room, Color.yellow, Time.deltaTime);
+        }
+    }
+
+    [Button]
+    void Redraw()
+    {
+        roomsToCheck.Clear();
+        Start();
     }
 }
